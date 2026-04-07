f14b5958ca8caacd3d960fb02e155a996bebb512
KBrand <kbrandn25@gmail.com>
2026-04-07 15:55:16 +0100

    some changes made in the signup page

--- src/components/signUp.tsx
+++ src/components/signUp.tsx
@@ -2,14 +2,14 @@ import Imghero from "../assets/hero.png";
 
 export default function SignUp() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-[2fr_1fr] gap-4 bg-linear-to-t from-white via-black/15 to-black-700 rounded-2xl shadow-2xl p-6">
+    <div className="grid grid-cols-1 md:grid-cols-[2fr_1fr] gap-4 bg-linear-to-t from-gray-100/70 via-black/10 to-gray-200/14 rounded-2xl shadow-2xl p-6">
       <div className="p-8 rounded-lg w-full ">
         <h2 className="font-extrabold text-3xl md:text-6xl">Join our culinary inner circle.</h2>
         <p className="text-gray-600 mt-2 mb-6">
           Get exclusive recipes, seasonal guides, and chef interviews delivered straight to your inbox. Sign up now and start cooking with confidence!
         </p>
         <div className="space-y-4 gap-3 w-full flex md:flex-row flex-col">
-            <input type="email" placeholder="email@address.com" className="w-full py-3 px-4 border border-gray-300 rounded-full shadow-sm focus:outline-none focus:ring-2 focus:ring-orange-500 focus:border-orange-500" />
+            <input type="email" placeholder="email@address.com" className="w-full py-3 px-4 border bg-white border-gray-300 rounded-full shadow-sm focus:outline-none focus:ring-2 focus:ring-orange-500 focus:border-orange-500" />
             <button type="submit" className="max-w-5xl h-fit cursor-pointer bg-black/90 text-white py-3 px-4 text-nowrap rounded-full hover:bg-black transition-colors duration-200 font-medium">
               Sign Me Up
             </button>
